8c721ed9c14dbdd1014856c6393a7f10c2bba1db
Mark Backhouse <markbackhouse@skyscanner.net>
2019-02-10 10:39:50 +0000

    Implement Bomb

--- src/main/java/moves/BombMove.java
+++ src/main/java/moves/BombMove.java
@@ -1,4 +1,47 @@
 package moves;
 
-public class BombMove {
+import models.CardList;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class BombMove implements Move {
+    private CardList cards;
+
+    public BombMove(CardList cards) {
+        this.cards = cards;
+    }
+
+    @Override
+    public boolean isValid() {
+        if (isJokerBomb()) {
+            return true;
+        }
+
+        Map<Integer, Long> valueFrequencies = new CardList(cards.getCards().stream()
+                .filter(c -> !c.isJoker())
+                .collect(Collectors.toList()))
+                .getFrequencies();
+
+        return cards.size() == 4 && (
+                valueFrequencies.containsValue(4)
+                        || valueFrequencies.containsValue(3) && cards.getJokerCount() == 1
+                        || valueFrequencies.containsValue(2) && cards.getJokerCount() == 2
+        );
+    }
+
+    @Override
+    public boolean beats(Move other) {
+        return true;
+    }
+
+    @Override
+    public MoveType getType() {
+        return MoveType.BOMB;
+    }
+
+    private boolean isJokerBomb() {
+        return cards.size() == 2
+                && cards.containsBothJokers();
+    }
 }
